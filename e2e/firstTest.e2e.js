372c6e2061d50d2672d035cc01656d81af0dceac
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-16 17:54:32 -0300

    feat: add detox

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,16 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp()
+  })
+
+  beforeEach(async () => {
+    await device.reloadReactNative()
+  })
+
+  it('should have welcome screen', async () => {
+    const info = element(
+      by.text('Open up App.js to start working on your app!!')
+    )
+    await expect(info).toBeVisible()
+  })
+})
